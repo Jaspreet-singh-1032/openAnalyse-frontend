3d1e83619ea8d4eebe838c0ba58dbf8789a34d68
Jaspreet-singh-1032 <jassimehra2612@gmail.com>
2021-12-30 23:09:31 +0530

    added react router and seperated home and app page

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Container from "@mui/material/Container";
+
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <Container>
+        <div className="home__mainText">
+          <h1>Keep learning! Keep Growing!</h1>
+        </div>
+      </Container>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Activity from "./Activity";
+import Visualize from "./Visualize";
+
+function Main() {
+  return (
+    <div>
+      <Activity />
+      <Visualize />
+    </div>
+  );
+}
+
+export default Main;
