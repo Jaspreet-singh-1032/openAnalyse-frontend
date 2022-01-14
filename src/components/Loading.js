7010819ddace57f36c30859ab36f4e705b466023
Jaspreet-singh-1032 <jassimehra2612@gmail.com>
2022-01-14 22:58:53 +0530

    changed progress bar color to success

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -6,7 +6,7 @@ import Box from "@mui/material/Box";
 function Loading() {
   return (
     <Box sx={{ width: "100%", position: "fixed", top: 0 }}>
-      <LinearProgress />
+      <LinearProgress color="success" />
     </Box>
   );
 }
