495e5f433e7add630ff15820114f91fe84735eaf
Jaspreet-singh-1032 <jassimehra2612@gmail.com>
2022-01-16 12:29:13 +0530

    remove unnecessary fetchdata function from useEffect

--- src/components/Visualize.js
+++ src/components/Visualize.js
@@ -11,30 +11,27 @@ function Visualize() {
   const { fetchActivityTypeActivities, state } = useContext(GlobalContext);
   const [chartData, setchartData] = useState({});
 
-  useEffect(() => {
-    async function fetchData() {
-      let response = await fetchActivityTypeActivities(state.chartFilter.days);
-      if (!response.detail) {
-        response = response.filter((item) => item.total_time_spent !== "0");
-        setchartData(() => {
-          return {
-            labels: response.map((item) => item.name),
-            datasets: [
-              {
-                label: "Hours invested",
-                data: response.map((item) =>
-                  parseFloat(item.total_time_spent / 60 / 60).toFixed(1)
-                ),
-                backgroundColor: response.map(
-                  () => "#" + Math.random().toString(16).substr(-6)
-                ),
-              },
-            ],
-          };
-        });
-      }
+  useEffect(async () => {
+    let response = await fetchActivityTypeActivities(state.chartFilter.days);
+    if (!response.detail) {
+      response = response.filter((item) => item.total_time_spent !== "0");
+      setchartData(() => {
+        return {
+          labels: response.map((item) => item.name),
+          datasets: [
+            {
+              label: "Hours invested",
+              data: response.map((item) =>
+                parseFloat(item.total_time_spent / 60 / 60).toFixed(1)
+              ),
+              backgroundColor: response.map(
+                () => "#" + Math.random().toString(16).substr(-6)
+              ),
+            },
+          ],
+        };
+      });
     }
-    fetchData();
   }, [fetchActivityTypeActivities, state.refreshGraph, state.chartFilter]);
   return (
     <div className="visualize">
